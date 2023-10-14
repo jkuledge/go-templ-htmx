15865d60f63646b00e79ec4f3ce1dad6247554ad
Josh Kuledge <josh.kuledge@ovative.com>
2023-10-14 10:37:26 -0500

    modifying a global state with a button

--- main.go
+++ main.go
@@ -3,14 +3,50 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/a-h/templ"
 )
 
-func main() {
-	component := hello("Josh")
+type GlobalState struct {
+	Count int
+}
 
-	http.Handle("/", templ.Handler(component))
+var global GlobalState
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	component := page(global.Count, 0)
+	component.Render(r.Context(), w)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	// Update state
+	r.ParseForm()
+
+	fmt.Println(r.Form.Get("user"))
+
+	// check if global button has been pressed
+	if r.Form.Has("global") {
+		global.Count++
+	}
+
+	// TODO: update session
+
+	// now that we've udpated state - rerender the page
+	getHandler(w, r)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		postHandler(w, r)
+		return
+	}
+	getHandler(w, r)
+}
+
+func main() {
+	// component := hello("Josh")
+
+	// http.Handle("/", templ.Handler(component))
+
+	http.HandleFunc("/", rootHandler)
 
 	fmt.Println("Listening on :3000")
 	http.ListenAndServe(":3000", nil)
